b503bcb962f94413c76204537e928f57fb220561
Flora2020 <rossignols1989@gmail.com>
2021-01-24 18:14:28 +0800

    feat: modify data source of show page

--- app.js
+++ app.js
@@ -35,11 +35,12 @@ app.get('/restaurants/new', (req, res) => {
   res.render('new')
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find((restaurant) => { return restaurant.id.toString() === req.params.restaurant_id })
-  console.log(req.params.restaurant_id)
-  console.log(restaurant)
-  res.render('show', { restaurant })
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
