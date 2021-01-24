addc5066ac02cd2d8008f3e3597028bfe13701fe
Flora2020 <rossignols1989@gmail.com>
2021-01-24 21:13:13 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -50,11 +50,39 @@ app.get('/search', (req, res) => {
   res.render('index', { restaurants, keyword: req.query.keyword })
 })
 
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
 app.post('/restaurants', (req, res) => {
   Restaurant.create(req.body)
     .then((restaurant) => {
       res.redirect('/')
     })
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const updatedRestaurant = req.body
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = updatedRestaurant.name
+      restaurant.name_en = updatedRestaurant.name_en
+      restaurant.category = updatedRestaurant.category
+      restaurant.image = updatedRestaurant.image
+      restaurant.location = updatedRestaurant.location
+      restaurant.phone = updatedRestaurant.phone
+      restaurant.google_map = updatedRestaurant.google_map
+      restaurant.rating = updatedRestaurant.rating
+      restaurant.description = updatedRestaurant.description
+      return restaurant.save()
+    })
+    .then(restaurant => res.redirect(`/restaurants/${restaurant._id}`))
     .catch(error => console.log(error))
 })
 
