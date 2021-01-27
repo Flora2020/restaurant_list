a4a357fd66e5e24c8af13e64023220e25bc4b082
Flora2020 <rossignols1989@gmail.com>
2021-01-27 23:26:11 +0800

    feat: add sorted search results function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,18 +11,79 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
-  Restaurant.find()
-    .lean()
-    .then(restaurants => {
-      return restaurants.filter(
-        (restaurant) => {
-          return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+  //在「sort({ 參數1: 參數2 })」中，參數 1 的位置似乎不能放變數，也不能放`${變數}`。因為參數 1 的位置要放的東西分別有 name、category、location，只好分成三個情況處理，但其他部分的程式碼都只是複製貼上。須再研究怎麼重構排序的程式碼
+  const sort = req.query.sort
+  let order = 'asc'
+  switch (sort) {
+    //參數 1 的位置放 name
+    case 'asc':
+    case 'desc':
+      order = sort
+      Restaurant.find()
+        .lean()
+        .sort({ name: order })
+        .then(restaurants => {
+          return restaurants.filter(
+            (restaurant) => {
+              return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+            })
         })
-    })
-    .then(restaurants => {
-      res.render('index', { restaurants, keyword })
-    })
-    .catch(error => console.log(error))
+        .then(restaurants => {
+          res.render('index', { restaurants, keyword })
+        })
+        .catch(error => console.log(error))
+      return
+
+    //參數 1 的位置放 category
+    case 'category':
+      Restaurant.find()
+        .lean()
+        .sort({ category: 'asc' })
+        .then(restaurants => {
+          return restaurants.filter(
+            (restaurant) => {
+              return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+            })
+        })
+        .then(restaurants => {
+          res.render('index', { restaurants, keyword })
+        })
+        .catch(error => console.log(error))
+      return
+
+    //參數 1 的位置放 location
+    case 'location':
+      Restaurant.find()
+        .lean()
+        .sort({ location: 'asc' })
+        .then(restaurants => {
+          return restaurants.filter(
+            (restaurant) => {
+              return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+            })
+        })
+        .then(restaurants => {
+          res.render('index', { restaurants, keyword })
+        })
+        .catch(error => console.log(error))
+      return
+
+    default:
+      console.log('exceptional case occur')
+      Restaurant.find()
+        .lean()
+        .then(restaurants => {
+          return restaurants.filter(
+            (restaurant) => {
+              return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+            })
+        })
+        .then(restaurants => {
+          res.render('index', { restaurants, keyword })
+        })
+        .catch(error => console.log(error))
+      return
+  }
 })
 
 module.exports = router
