bee518f551de6d69210d2999a0a8459acf3eab68
Flora2020 <rossignols1989@gmail.com>
2021-02-26 19:30:12 +0800

    feat: add relation between User and Restaurant models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,13 +3,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  Restaurant.find()
+  const userId = req.user._id
+  Restaurant.find({ userId })
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
 
 router.get('/search', (req, res) => {
+  const userId = req.user._id
   const keyword = req.query.keyword.trim().toLowerCase()
   //在「sort({ 參數1: 參數2 })」中，參數 1 的位置似乎不能放變數，也不能放`${變數}`。因為參數 1 的位置要放的東西分別有 name、category、location，只好分成三個情況處理，但其他部分的程式碼都只是複製貼上。須再研究怎麼重構排序的程式碼
   const sort = req.query.sort
@@ -19,7 +21,7 @@ router.get('/search', (req, res) => {
     case 'asc':
     case 'desc':
       order = sort
-      Restaurant.find()
+      Restaurant.find({ userId })
         .lean()
         .sort({ name: order })
         .then(restaurants => {
@@ -36,7 +38,7 @@ router.get('/search', (req, res) => {
 
     //參數 1 的位置放 category
     case 'category':
-      Restaurant.find()
+      Restaurant.find({ userId })
         .lean()
         .sort({ category: 'asc' })
         .then(restaurants => {
@@ -53,7 +55,7 @@ router.get('/search', (req, res) => {
 
     //參數 1 的位置放 location
     case 'location':
-      Restaurant.find()
+      Restaurant.find({ userId })
         .lean()
         .sort({ location: 'asc' })
         .then(restaurants => {
@@ -70,7 +72,7 @@ router.get('/search', (req, res) => {
 
     default:
       console.log('exceptional case occur')
-      Restaurant.find()
+      Restaurant.find({ userId })
         .lean()
         .then(restaurants => {
           return restaurants.filter(
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -7,22 +7,25 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const _id = req.params.id
+  const userId = req.user._id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const _id = req.params.id
+  const userId = req.user._id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
     .catch(error => console.log(error))
 })
 
 router.post('/', (req, res) => {
+  req.body.userId = req.user._id
   Restaurant.create(req.body)
     .then((restaurant) => {
       res.redirect('/')
@@ -31,9 +34,10 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const _id = req.params.id
+  const userId = req.user._id
   const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  return Restaurant.findById(id)
+  return Restaurant.findOne({ _id, userId })
     .then((restaurant) => {
       restaurant.name = name
       restaurant.name_en = name_en
@@ -51,8 +55,9 @@ router.put('/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const _id = req.params.id
+  const userId = req.user._id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurant => {
       return restaurant.remove()
     })
