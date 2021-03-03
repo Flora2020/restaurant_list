b98c69404eded023d44f6520726c17e479b518f2
Flora2020 <rossignols1989@gmail.com>
2021-03-03 10:13:02 +0800

    refactor: sorting restaurants

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -13,79 +13,34 @@ router.get('/', (req, res) => {
 router.get('/search', (req, res) => {
   const userId = req.user._id
   const keyword = req.query.keyword.trim().toLowerCase()
-  //在「sort({ 參數1: 參數2 })」中，參數 1 的位置似乎不能放變數，也不能放`${變數}`。因為參數 1 的位置要放的東西分別有 name、category、location，只好分成三個情況處理，但其他部分的程式碼都只是複製貼上。須再研究怎麼重構排序的程式碼
   const sort = req.query.sort
   let order = 'asc'
+  let field = 'name'
+
   switch (sort) {
-    //參數 1 的位置放 name
-    case 'asc':
     case 'desc':
       order = sort
-      Restaurant.find({ userId })
-        .lean()
-        .sort({ name: order })
-        .then(restaurants => {
-          return restaurants.filter(
-            (restaurant) => {
-              return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
-            })
-        })
-        .then(restaurants => {
-          res.render('index', { restaurants, keyword })
-        })
-        .catch(error => console.log(error))
-      return
-
-    //參數 1 的位置放 category
+      break
     case 'category':
-      Restaurant.find({ userId })
-        .lean()
-        .sort({ category: 'asc' })
-        .then(restaurants => {
-          return restaurants.filter(
-            (restaurant) => {
-              return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
-            })
-        })
-        .then(restaurants => {
-          res.render('index', { restaurants, keyword })
-        })
-        .catch(error => console.log(error))
-      return
-
-    //參數 1 的位置放 location
     case 'location':
-      Restaurant.find({ userId })
-        .lean()
-        .sort({ location: 'asc' })
-        .then(restaurants => {
-          return restaurants.filter(
-            (restaurant) => {
-              return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
-            })
-        })
-        .then(restaurants => {
-          res.render('index', { restaurants, keyword })
-        })
-        .catch(error => console.log(error))
-      return
-
+      field = sort
+      break
     default:
-      console.log('exceptional case occur')
-      Restaurant.find({ userId })
-        .lean()
-        .then(restaurants => {
-          return restaurants.filter(
-            (restaurant) => {
-              return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
-            })
-        })
-        .then(restaurants => {
-          res.render('index', { restaurants, keyword })
-        })
-        .catch(error => console.log(error))
-      return
   }
+
+  Restaurant.find({ userId })
+    .sort({ [field]: order })
+    .lean()
+    .then(restaurants => {
+      return restaurants.filter(
+        (restaurant) => {
+          return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+        })
+    })
+    .then(restaurants => {
+      res.render('index', { restaurants, keyword })
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
